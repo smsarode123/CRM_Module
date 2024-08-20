34bf705cb4afa36db48c8f1a7ab1b896103a00ed
Harish4216 <shindeharish229@gmail.com>
2024-08-20 17:19:37 +0530

    vehicle model class push

--- InsuranceRegistrationService/src/main/java/com/insurance/registrationservice/model/Vehicle.java
+++ InsuranceRegistrationService/src/main/java/com/insurance/registrationservice/model/Vehicle.java
@@ -1,6 +1,28 @@
 package com.insurance.registrationservice.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Vehicle {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int vehicleid;
+	private int vehiclercnumber;
+	private String vehicleownername;
+	private String vehicletype;
+	private String vehiclemanufacturename;
+	private String vehiclemodel;
+	private String vehicleRegistrationDate;
+	private String vehicleRegistrationUpto;
 
 }
