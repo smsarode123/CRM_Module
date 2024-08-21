013c5a1234e0ef97ff8cd618a0ac5391fb1b1ab3
smssarode123 <sagarsarode1997@gmail.com>
2024-08-21 12:50:07 +0530

    Customer Api added by sagar

--- InsuranceRegistrationService/src/main/java/com/insurance/registrationservice/serviceimpl/CustomerServiceImpl.java
+++ InsuranceRegistrationService/src/main/java/com/insurance/registrationservice/serviceimpl/CustomerServiceImpl.java
@@ -1,9 +1,11 @@
 package com.insurance.registrationservice.serviceimpl;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.insurance.registrationservice.exception.InvalidVehicleIdException;
 import com.insurance.registrationservice.model.Customer;
 import com.insurance.registrationservice.repository.CustomerRepository;
 import com.insurance.registrationservice.service.CustomerServiceI;
@@ -23,6 +25,18 @@ public class CustomerServiceImpl implements CustomerServiceI{
 		
 		return repository.findAll();
 	}
+
+	@Override
+	public Customer getSingleVehicleById(int customerId) {
+		
+		Optional<Customer> customer = repository.findById(customerId);
+
+		if (customer.isPresent()) {
+
+			return customer.get();
+		} else {
+			throw new InvalidVehicleIdException("Vehicle id " + customerId + " is not valid");
+		}	}
 
 
 }
