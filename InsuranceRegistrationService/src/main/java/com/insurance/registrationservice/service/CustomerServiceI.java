4d8a5c76ce657831513f63e2373a1eb05152b165
smssarode123 <sagarsarode1997@gmail.com>
2024-08-22 09:42:08 +0530

    Customer Apis added by sagar

--- InsuranceRegistrationService/src/main/java/com/insurance/registrationservice/service/CustomerServiceI.java
+++ InsuranceRegistrationService/src/main/java/com/insurance/registrationservice/service/CustomerServiceI.java
@@ -2,6 +2,27 @@ package com.insurance.registrationservice.service;
 
 import java.util.List;
 
+import com.insurance.registrationservice.model.Customer;
+import com.insurance.registrationservice.model.Vehicle;
 
+public interface CustomerServiceI {
+
+	Customer saveCustomers(Customer customer);
+
+	List<Customer> getAllCustomer();
+
+	Vehicle insertdataofcustomer(Vehicle vehicle);
+
+	Iterable<Vehicle> SelectAllVehicle();
+
+	void RemoveVehicleById(int vehicleid);
+
+	Vehicle UpdateVehicleData(Vehicle vehicle, int vehicleid);
+
+	Customer getSingleCustomerById(int customerId);
+
+	Customer updateCustomer(Customer customer, int customerId);
+
+	void deleteCustomer(int customerId);
 
 }
--- InsuranceRegistrationService/src/main/java/com/insurance/registrationservice/serviceimpl/CustomerServiceImpl.java
+++ InsuranceRegistrationService/src/main/java/com/insurance/registrationservice/serviceimpl/CustomerServiceImpl.java
@@ -1,76 +1,110 @@
 package com.insurance.registrationservice.serviceimpl;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.stereotype.Service;
+
+import com.insurance.registrationservice.exception.InvalidCustomerIdException;
+import com.insurance.registrationservice.model.Customer;
+import com.insurance.registrationservice.model.Vehicle;
 import com.insurance.registrationservice.repository.CustomerRepository;
 import com.insurance.registrationservice.repository.VehicleRepository;
 import com.insurance.registrationservice.service.CustomerServiceI;
+
 @Service
-public class CustomerServiceImpl implements CustomerServiceI{
-	
+public class CustomerServiceImpl implements CustomerServiceI {
+
 	private CustomerRepository repository;
 	private VehicleRepository vehiclerepository;
 
 	@Override
 	public Vehicle insertdataofcustomer(Vehicle vehicle) {
-		
-	     Vehicle vehicledata=vehiclerepository.save(vehicle);
-		
+
+		Vehicle vehicledata = vehiclerepository.save(vehicle);
+
 		return vehicledata;
 	}
 
 	@Override
 	public Iterable<Vehicle> SelectAllVehicle() {
-		
-		Iterable<Vehicle>vehicledata=vehiclerepository.findAll();
-		
-		
+
+		Iterable<Vehicle> vehicledata = vehiclerepository.findAll();
+
 		return vehicledata;
 	}
 
 	@Override
 	public void RemoveVehicleById(int vehicleid) {
-		
+
 		vehiclerepository.deleteById(vehicleid);
-		
-	
+
 	}
 
 	@Override
 	public Vehicle UpdateVehicleData(Vehicle vehicle, int vehicleid) {
-		
-	     vehiclerepository.findById(vehicleid);
-		
-	Vehicle vehicledata=vehiclerepository.save(vehicle);
-		
+
+		vehiclerepository.findById(vehicleid);
+
+		Vehicle vehicledata = vehiclerepository.save(vehicle);
+
 		return vehicledata;
-	}
 
-	
-
-	@Override
-	public Customer saveCustomers(Customer customer) {
-		
-		return repository.save(customer);
 	}
 
 	@Override
 	public List<Customer> getAllCustomer() {
-		
-		return repository.findAll();
+
+		List<Customer> list = repository.findAll();
+
+		return list;
 	}
 
 	@Override
-	public Customer getSingleVehicleById(int customerId) {
-		
+	public Customer saveCustomers(Customer customer) {
+		return repository.save(customer);
+	}
+
+	@Override
+	public Customer getSingleCustomerById(int customerId) {
+
 		Optional<Customer> customer = repository.findById(customerId);
 
 		if (customer.isPresent()) {
 
 			return customer.get();
 		} else {
-			throw new InvalidVehicleIdException("Vehicle id " + customerId + " is not valid");
-		}	}
+			throw new InvalidCustomerIdException("Customer id " + customerId + " is not valid");
+		}
+	}
 
+	@Override
+	public Customer updateCustomer(Customer customer, int customerId) {
+		Optional<Customer> customerRef =repository.findById(customerId);
+		
+		if(customerRef.isPresent()) {
+			return repository.save(customer);
+		}
+		
+		else {
+			throw new InvalidCustomerIdException("Customer id " + customerId + " is not valid");
+		}
+		
+	}
+
+	@Override
+	public void deleteCustomer(int customerId) {
+		
+		Optional<Customer> customerRef=repository.findById(customerId);
+		
+		if(customerRef.isPresent()) 
+		{
+			repository.deleteById(customerId);
+		}
+		else {
+			throw new InvalidCustomerIdException("Customer id " + customerId + " is not valid");
+ 
+		}
+	}
 
 }
